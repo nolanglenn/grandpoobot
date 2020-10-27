f7e218783a54021be94b5f0d3315716ed09acd1f
unknown <nolangglenn@gmail.com>
2020-10-27 17:43:35 -0500

    Bot connects to channel.

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import tmi from 'tmi.js'
+require('dotenv').config()
+
+const client = new tmi.Client({
+	options: { debug: true },
+	connection: {
+		secure: true,
+		reconnect: true
+	},
+	identity: {
+		username: process.env.BOT_USERNAME,
+		password: process.env.OAUTH_TOKEN
+	},
+	channels: [ process.env.CHANNEL_NAME ]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	// Ignore echoed messages.
+	if(self) return;
+
+	if(message.toLowerCase() === '!hello') {
+		// "@alca, heya!"
+		client.say(channel, `@${tags.username}, heya!`);
+	}
+});
